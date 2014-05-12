01bddffeb48845888ec22bde6bf0c32ae0c0725d
Adam Edgett <AdamEdgett@gmail.com>
2014-05-12 10:39:05 -0400

    Finished brute force for knapsack

--- P1/p1a.cpp
+++ P1/p1a.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <cstdlib>
 #include <limits.h>
 #include <list>
 #include <fstream>
@@ -16,47 +17,92 @@ using namespace std;
 #include "graph.h"
 #include "knapsack.h"
 
-int main()
+vector<int> bestSolution;
+int bestSolutionValue;
+
+void saveSolution(knapsack &k)
 {
-   char x;
-   ifstream fin;
-   stack <int> moves;
-   string fileName;
-   
-   // Read the name of the graph from the keyboard or
-   // hard code it here for testing.
-   
-   // fileName = "knapsack16.input";
+	bestSolution.clear();
+	for(int i = 0; i < k.getNumObjects(); i++)
+	{
+		if(k.isSelected(i))
+			bestSolution.push_back(i);
+	}
+	bestSolutionValue = k.getValue();
+}
 
-   cout << "Enter filename" << endl;
-   cin >> fileName;
-   
-   fin.open(fileName.c_str());
-   if (!fin)
-   {
-      cerr << "Cannot open " << fileName << endl;
-      exit(1);
-   }
+void iterateKnapsack(knapsack &k, int currentIndex)
+{
+	if(k.getCost() < k.getCostLimit())
+	{
+		if(currentIndex < k.getNumObjects())
+		{
+			k.select(currentIndex);
+			iterateKnapsack(k, currentIndex + 1);
+			k.unSelect(currentIndex);
+			iterateKnapsack(k, currentIndex + 1);
+		}
+		else if(k.getValue() >= bestSolutionValue)
+		{
+			saveSolution(k);
+		}
+	}
+}
 
-   try
-   {
-      cout << "Reading knapsack instance" << endl;
-      knapsack k(fin);
+void exhaustiveKnapsack(knapsack &k, int t)
+{
+	iterateKnapsack(k, 0);
+	for(int i = 0; i < bestSolution.size(); i++)
+	{
+		k.select(bestSolution[i]);
+	}
+	cout << "Cost limit: " << k.getCostLimit() << "\n";
+	return;
+}
 
-      exhaustiveKnapsack(k, 600);
+int main(int argc, char* argv[])
+{
+	char x;
+	ifstream fin;
+	stack <int> moves;
+	string fileName;
 
-      cout << endl << "Best solution" << endl;
-      k.printSolution();
-      
-   }    
+	// Read the name of the graph from the keyboard or
+	// hard code it here for testing.
 
-   catch (indexRangeError &ex) 
-   { 
-      cout << ex.what() << endl; exit(1);
-   }
-   catch (rangeError &ex)
-   {
-      cout << ex.what() << endl; exit(1);
-   }
+	fileName = argv[1];
+
+	if(fileName.empty())
+	{
+		cout << "Enter filename" << endl;
+		cin >> fileName;
+	}
+
+	fin.open(fileName.c_str());
+	if (!fin)
+	{
+		cerr << "Cannot open " << fileName << endl;
+		exit(1);
+	}
+
+	try
+	{
+		cout << "Reading knapsack instance" << endl;
+		knapsack k(fin);
+
+		exhaustiveKnapsack(k, 600);
+
+		cout << endl << "Best solution" << endl;
+		k.printSolution();
+	}
+
+	catch (indexRangeError &ex)
+	{
+		cout << ex.what() << endl; exit(1);
+	}
+	catch (rangeError &ex)
+	{
+		cout << ex.what() << endl; exit(1);
+	}
 }
 
