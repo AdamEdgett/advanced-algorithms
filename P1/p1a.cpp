73c6cc3cd4bcdd708293eb17c716632f249f6522
Adam Edgett <AdamEdgett@gmail.com>
2014-05-12 13:58:11 -0400

    Fixed up solution for knapsack

--- P1/p1a.cpp
+++ P1/p1a.cpp
@@ -17,48 +17,18 @@ using namespace std;
 #include "graph.h"
 #include "knapsack.h"
 
+static time_t t;
+static time_t start;
+static int timeLimit;
 vector<int> bestSolution;
 int bestSolutionValue;
 
-void saveSolution(knapsack &k)
-{
-	bestSolution.clear();
-	for(int i = 0; i < k.getNumObjects(); i++)
-	{
-		if(k.isSelected(i))
-			bestSolution.push_back(i);
-	}
-	bestSolutionValue = k.getValue();
-}
-
-void iterateKnapsack(knapsack &k, int currentIndex)
-{
-	if(k.getCost() < k.getCostLimit())
-	{
-		if(currentIndex < k.getNumObjects())
-		{
-			k.select(currentIndex);
-			iterateKnapsack(k, currentIndex + 1);
-			k.unSelect(currentIndex);
-			iterateKnapsack(k, currentIndex + 1);
-		}
-		else if(k.getValue() >= bestSolutionValue)
-		{
-			saveSolution(k);
-		}
-	}
-}
-
-void exhaustiveKnapsack(knapsack &k, int t)
-{
-	iterateKnapsack(k, 0);
-	for(int i = 0; i < bestSolution.size(); i++)
-	{
-		k.select(bestSolution[i]);
-	}
-	cout << "Cost limit: " << k.getCostLimit() << "\n";
-	return;
-}
+void exhaustiveKnapsack(knapsack &k, int t);
+void interateKnapsack(knapsack &k, int currentIndex);
+void saveSolution(knapsack &k);
+void loadSolution(knapsack &k);
+void checkTimeLimit(knapsack &k);
+void outputSolution(knapsack &k);
 
 int main(int argc, char* argv[])
 {
@@ -103,6 +73,94 @@ int main(int argc, char* argv[])
 	catch (rangeError &ex)
 	{
 		cout << ex.what() << endl; exit(1);
+	}
+	catch (baseException &ex)
+	{
+		cout << ex.what() << endl; exit(1);
+	}
+}
+
+/**
+ * Recursively test all possible solutions for the knapsack
+ * @param k the knapsack to use
+ * @param currentIndex the current item
+ */
+void iterateKnapsack(knapsack &k, int currentIndex)
+{
+	if(k.getCost() < k.getCostLimit())
+	{
+		checkTimeLimit(k);
+		if(currentIndex < k.getNumObjects())
+		{
+			k.select(currentIndex);
+			iterateKnapsack(k, currentIndex + 1);
+			k.unSelect(currentIndex);
+			iterateKnapsack(k, currentIndex + 1);
+		}
+		else if(k.getValue() >= bestSolutionValue)
+		{
+			saveSolution(k);
+		}
+	}
+}
+
+/**
+ * Uses brute force to find the best solution
+ * @param k the knapsack to use
+ * @param t the time limit
+ */
+void exhaustiveKnapsack(knapsack &k, int t)
+{
+	start = time(0);
+	timeLimit = t;
+	cout << "Cost limit: " << k.getCostLimit() << endl;
+	iterateKnapsack(k, 0);
+	loadSolution(k);
+}
+
+/**
+ * Saves the current solution to a solution vector
+ * @param k the knapsack to use
+ */
+void saveSolution(knapsack &k)
+{
+	bestSolution.clear();
+	for(int i = 0; i < k.getNumObjects(); i++)
+	{
+		if(k.isSelected(i))
+			bestSolution.push_back(i);
+	}
+	bestSolutionValue = k.getValue();
+}
+
+/**
+ * Loads the solution from the solution vector
+ * @param k the knapsack to use
+ */
+void loadSolution(knapsack &k)
+{
+	for(int i = 0; i < k.getNumObjects(); i++)
+	{
+		k.unSelect(i);
+	}
+	for(int i = 0; i < bestSolution.size(); i++)
+	{
+		k.select(bestSolution[i]);
+	}
+}
+
+/**
+ * Checks the time limit and ends the program if it is exceeded
+ * @param k the knapsack to use
+ */
+void checkTimeLimit(knapsack &k)
+{
+	t = time(0);
+	if(t - start > timeLimit)
+	{
+		loadSolution(k);
+		k.printSolution();
+		throw baseException("========== Time expired ==========");
 	}
 }
 
