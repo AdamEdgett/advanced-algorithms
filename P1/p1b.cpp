a6e6f10ff3b1df15de40bd32110f73cf08dbae95
mcdm99 <mcdm99@gmail.com>
2014-05-12 00:53:06 -0400

    SANE spacing (Autoindent tabs)

--- P1/p1b.cpp
+++ P1/p1b.cpp
@@ -19,56 +19,63 @@ void exhaustiveColoring(graph g, int numColors, int limit);
 
 int main()
 {
-   char x;
-   ifstream fin;
-   stack <int> moves;
-   string fileName;
-   int numColors;
-   vector<int> solution;
+	char x;
+	ifstream fin;
+	stack <int> moves;
+	string fileName;
+	int numColors;
+	vector<int> solution;
 
-   // Read the name of the graph from the keyboard or
-   // hard code it here for testing.
-   
-   fileName = "../instances/color/color12-4.input";
+	// Read the name of the graph from the keyboard or
+	// hard code it here for testing.
 
-   //cout << "Enter filename" << endl;
-   //cin >> fileName;
-   
-   fin.open(fileName.c_str());
-   if (!fin)
-   {
-      cerr << "Cannot open " << fileName << endl;
-      return 1;
-   }
+	fileName = "../instances/color/color12-4.input";
 
-   try
-   {
-      cout << "Reading number of colors" << endl;
-      fin >> numColors;
-	 
-      cout << "Reading graph" << endl;
-      graph g(fin);
+	//cout << "Enter filename" << endl;
+	//cin >> fileName;
 
-      cout << "Num colors: " << numColors << endl;
-      cout << g;
+	fin.open(fileName.c_str());
+	if (!fin)
+	{
+		cerr << "Cannot open " << fileName << endl;
+		return 1;
+	}
 
-      exhaustiveColoring(g, numColors, 60);
-      g.printEdges();
-      g.printNodes();
-   }    
-   catch (indexRangeError &ex) 
-   { 
-      cout << ex.what() << endl; 
-      return 1;
-   }
-   catch (rangeError &ex)
-   {
-      cout << ex.what() << endl; 
-      return 1;
-   }
+	try
+	{
+		cout << "Reading number of colors" << endl;
+		fin >> numColors;
+
+		cout << "Reading graph" << endl;
+		graph g(fin);
+
+		cout << "Num colors: " << numColors << endl;
+		cout << g;
+
+		exhaustiveColoring(g, numColors, 60);
+		//g.printEdges();
+		g.printNodes();
+	}    
+	catch (indexRangeError &ex) 
+	{ 
+		cout << ex.what() << endl; 
+		return 1;
+	}
+	catch (rangeError &ex)
+	{
+		cout << ex.what() << endl; 
+		return 1;
+	}
 }
-    
+
+bool isColored(graph g)
+{
+	return false;
+}
+
 void exhaustiveColoring(graph g, int numColors, int limit)
 {
-
+	while(!isColored(g)){
+		
+	}
 }
