6b9ef35c7f69cfdff092be6759cbedab6d6e3d15
Adam Edgett <AdamEdgett@gmail.com>
2014-05-19 13:50:02 -0400

    Added results for P2A

--- P2/run_all.bash
+++ P2/run_all.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 #Run on all instance data conveniantly
 
-#./p2a.out ../instances/knapsack/knapsack1024.input > ./results/knapsack_1024.output
-#./p2a.out ../instances/knapsack/knapsack128.input > ./results/knapsack_128.output
-#./p2a.out ../instances/knapsack/knapsack12.input > ./results/knapsack_12.output
-#./p2a.out ../instances/knapsack/knapsack16.input > ./results/knapsack_16.output
-#./p2a.out ../instances/knapsack/knapsack20.input > ./results/knapsack_20.output
-#./p2a.out ../instances/knapsack/knapsack256.input > ./results/knapsack_256.output
-#./p2a.out ../instances/knapsack/knapsack28.input > ./results/knapsack_28.output
-#./p2a.out ../instances/knapsack/knapsack32.input > ./results/knapsack_32.output
-#./p2a.out ../instances/knapsack/knapsack48.input > ./results/knapsack_48.output
-#./p2a.out ../instances/knapsack/knapsack512.input > ./results/knapsack_512.output
-#./p2a.out ../instances/knapsack/knapsack64.input > ./results/knapsack_64.output
-#./p2a.out ../instances/knapsack/knapsack8.input > ./results/knapsack_8.output
+./p2a.out ../instances/knapsack/knapsack1024.input > ./results/knapsack_1024.output
+./p2a.out ../instances/knapsack/knapsack128.input > ./results/knapsack_128.output
+./p2a.out ../instances/knapsack/knapsack12.input > ./results/knapsack_12.output
+./p2a.out ../instances/knapsack/knapsack16.input > ./results/knapsack_16.output
+./p2a.out ../instances/knapsack/knapsack20.input > ./results/knapsack_20.output
+./p2a.out ../instances/knapsack/knapsack256.input > ./results/knapsack_256.output
+./p2a.out ../instances/knapsack/knapsack28.input > ./results/knapsack_28.output
+./p2a.out ../instances/knapsack/knapsack32.input > ./results/knapsack_32.output
+./p2a.out ../instances/knapsack/knapsack48.input > ./results/knapsack_48.output
+./p2a.out ../instances/knapsack/knapsack512.input > ./results/knapsack_512.output
+./p2a.out ../instances/knapsack/knapsack64.input > ./results/knapsack_64.output
+./p2a.out ../instances/knapsack/knapsack8.input > ./results/knapsack_8.output
 
 ./p2b.out ../instances/color/color12-3.input > ./results/color_12_3.output
 ./p2b.out ../instances/color/color12-4.input > ./results/color_12_4.output
