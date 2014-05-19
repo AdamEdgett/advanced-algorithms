412e63ed6c5b7d8c8a35f91dd0417d76bc1653f2
mcdm99 <mcdm99@gmail.com>
2014-05-19 01:48:26 -0400

    Adding P2 run_all script with P2B results

---
+++ P2/run_all.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Run on all instance data conveniantly
+
+#./p2a.out ../instances/knapsack/knapsack1024.input > ./results/knapsack_1024.output
+#./p2a.out ../instances/knapsack/knapsack128.input > ./results/knapsack_128.output
+#./p2a.out ../instances/knapsack/knapsack12.input > ./results/knapsack_12.output
+#./p2a.out ../instances/knapsack/knapsack16.input > ./results/knapsack_16.output
+#./p2a.out ../instances/knapsack/knapsack20.input > ./results/knapsack_20.output
+#./p2a.out ../instances/knapsack/knapsack256.input > ./results/knapsack_256.output
+#./p2a.out ../instances/knapsack/knapsack28.input > ./results/knapsack_28.output
+#./p2a.out ../instances/knapsack/knapsack32.input > ./results/knapsack_32.output
+#./p2a.out ../instances/knapsack/knapsack48.input > ./results/knapsack_48.output
+#./p2a.out ../instances/knapsack/knapsack512.input > ./results/knapsack_512.output
+#./p2a.out ../instances/knapsack/knapsack64.input > ./results/knapsack_64.output
+#./p2a.out ../instances/knapsack/knapsack8.input > ./results/knapsack_8.output
+
+./p2b.out ../instances/color/color12-3.input > ./results/color_12_3.output
+./p2b.out ../instances/color/color12-4.input > ./results/color_12_4.output
+./p2b.out ../instances/color/color192-6.input > ./results/color_192_6.output
+./p2b.out ../instances/color/color192-7.input > ./results/color_192_7.output
+./p2b.out ../instances/color/color192-8.input > ./results/color_192_8.output
+./p2b.out ../instances/color/color24-4.input > ./results/color_24_4.output
+./p2b.out ../instances/color/color24-5.input > ./results/color_24_5.output
+./p2b.out ../instances/color/color48-5.input > ./results/color_48_5.output
+./p2b.out ../instances/color/color48-6.input > ./results/color_48_6.output
+./p2b.out ../instances/color/color96-6.input > ./results/color_96_6.output
+./p2b.out ../instances/color/color96-7.input > ./results/color_96_7.output
+
